388c6e0b6ac1df90c269a64f908d69b25e0f1015
Rahil Sinha <rshzb14.7@gmail.com>
2021-10-25 14:34:55 +0530

    Implemented Chat functionality

--- server.js
+++ server.js
@@ -2,13 +2,37 @@ const express = require('express')
 const path = require('path')
 const socketio = require('socket.io')
 const http = require('http')
-
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+const formatMessage = require('./utils/messages')
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
+
+const botName = 'T-Chat Bot'
+
+// Run when client connects
+io.on('connection', (socket) => {
+  // Welcome current user
+  socket.emit('message', formatMessage(botName, 'Welcome to TChat!'))
+
+  // Broadcast when a user connects
+  socket.broadcast.emit(
+    'message',
+    formatMessage(botName, 'A user has joined the chat'),
+  )
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', formatMessage(botName, 'A user has left the chat'))
+  })
+
+  // Listen for chatMessage
+  socket.on('chatMessage', (msg) => {
+    io.emit('message', formatMessage('USER', msg))
+  })
+})
 
 const PORT = process.env.PORT || 3000
 
